e2390e42dfaace03b07bcc6f6ba30107ff7083d3
fanbook-wangdage <124357765+fanbook-wangdage@users.noreply.github.com>
2024-09-16 18:02:54 +0800

    Update and rename _test.py to test_something.py

---
+++ test_something.py
@@ -0,0 +1,7 @@
+def test_example_function():
+  import fanbookbotapi
+  token=''
+  print(fanbookbotapi.getme(token).text)
+  print(fanbookbotapi.sendmessage(token,chatid=433212507046281216).text)
+  print(fanbookbotapi.getPrivateChat(token,userid=389320179948986368).text)
+  fanbookbotapi.send_user_message(bot_token=token,channel_id='433212507046281216',guild_id='433204455396081664',text='{\"type\":\"text\",\"text\":\"6\",\"contentType\":0}')
